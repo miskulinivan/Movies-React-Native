7629480eadb4c37b011bd71ed38ddef02b343e36
miskulinivan <ivan.miskulin2701@gmail.com>
2023-09-07 17:29:13 +0200

    Add new image into assets

--- data/data.js
+++ data/data.js
@@ -1,11 +1,12 @@
 import person from '../assets/person.jpg';
 import ahsoka from '../assets/ahsoka.png';
 import lotr from '../assets/lotr.jpg';
-
+import wanda from '../assets/wanda.jpg';
 export const categories = ['all', 'movies', 'series', 'test', 'jabuka', 'aaaaaaaaa'];
 
 export const allData = [
     { id: 1, title: 'Person', imageUri: person, category: 'movies' },
     { id: 2, title: 'Ahsoka', imageUri: ahsoka, category: 'series' },
     { id: 3, title: 'Lord of the Rings', imageUri: lotr, category: 'movies' },
+    { id: 4, title: 'WandaVision', imageUri: wanda, category: 'series' },
 ];
